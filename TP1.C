e6e6b539d0e39362a9b86f5f79abbd69524c3cba
Roudina <roudinadacher@gmail.com>
2025-01-03 00:50:29 +0100

    Update TP1.C
    Code in C programming language that performs all mathematical operations (addition, subtraction, division, multiplication).

--- TP1.C
+++ TP1.C
@@ -3,14 +3,10 @@
 int main() {
     char operator;
     double num1, num2, result;
-
-    // نطلعلو من المستخدسم يدخل عملية لي حابها 
     printf("Enter an operator (+, -, *, /): ");
     scanf(" %c", &operator);
     printf("Enter two numbers: ");
     scanf("%lf %lf", &num1, &num2);
-
-    // هذي باش نمدولو نتيجة برك لواش كان حاب ...وحدة من العمليات فقط 
     switch (operator) {
         case '+':
             result = num1 + num2;
@@ -28,15 +24,14 @@ int main() {
             break;
 
         case '/':
-            // نتأكدو بلي المقام ماشي 0 باش نقسمو
+    
             if (num2 != 0) {
                 result = num1 / num2;
                 printf("%.2lf / %.2lf = %.2lf\n", num1, num2, result);
             } else {
                 printf("Error: Division by zero is undefined.\n");
             }
-            break;
-//هذي في حالة مدخلش عملية
+            break
         default:
             printf("Error: Invalid operator.\n");
             break;
